ad5023e75b910b050476f03462239d25d8048566
Javier  Gutierrez De La Fuente <jguti@alpha46.aulas.gsyc.urjc.es>
2017-10-02 13:03:19 +0200

    listo con pep8

--- calcoo.py
+++ calcoo.py
@@ -3,21 +3,21 @@
 
 import sys
 
+
 class Calculadora():
 
-    def plus(self, op1,op2):
-    
+    def plus(self, op1, op2):
+
         return op1 + op2
 
-
-    def minus(self,op1, op2):
+    def minus(self, op1, op2):
 
         return op1 - op2
 
 if __name__ == "__main__":
-    
+
     datos = Calculadora()
-    
+
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
